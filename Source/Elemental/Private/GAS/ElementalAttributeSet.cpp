46fec91105313879f3f812730e100eb441d63f6b
PauloGN <paulorsantos.gnt@outlook.com>
2024-06-01 18:36:02 -0700

    step 23-24 Attributes
    created two attributes Health and Mana along their max attributes
    following the boilerplate it is already setup for replication

--- Source/Elemental/Private/GAS/ElementalAttributeSet.cpp
+++ Source/Elemental/Private/GAS/ElementalAttributeSet.cpp
@@ -2,4 +2,41 @@
 
 
 #include "GAS/ElementalAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UElementalAttributeSet::UElementalAttributeSet()
+{
+}
+
+void UElementalAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//Vital Attributes
+	DOREPLIFETIME_CONDITION_NOTIFY(UElementalAttributeSet, Health, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UElementalAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UElementalAttributeSet, Mana, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UElementalAttributeSet, MaxMana, COND_None, REPNOTIFY_Always)
+}
+
+void UElementalAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UElementalAttributeSet, Health, OldHealth)
+}
+
+void UElementalAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UElementalAttributeSet, MaxHealth, OldMaxHealth)
+
+}
+
+void UElementalAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UElementalAttributeSet, Mana, OldMana)
+}
+
+void UElementalAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UElementalAttributeSet, MaxMana, OldMaxMana)
+}
--- Source/Elemental/Public/GAS/ElementalAttributeSet.h
+++ Source/Elemental/Public/GAS/ElementalAttributeSet.h
@@ -13,5 +13,36 @@ UCLASS()
 class ELEMENTAL_API UElementalAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UElementalAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Health, BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Mana, BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+
 };
